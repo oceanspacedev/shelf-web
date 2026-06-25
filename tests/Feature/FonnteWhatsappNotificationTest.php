978908fad997a74e812a3cf5d7b956a9009f363a
apriansyahrs <apriansyahrizs@gmail.com>
2026-06-26 06:25:44 +0700

    test: add reminder hardening cases

--- tests/Feature/FonnteWhatsappNotificationTest.php
+++ tests/Feature/FonnteWhatsappNotificationTest.php
@@ -3,6 +3,10 @@
 namespace Tests\Feature;
 
 use App\Console\Commands\SendScheduledNotifications;
+use App\Models\Asset;
+use App\Models\CustomAssetAttribute;
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use ReflectionMethod;
 use Tests\TestCase;
@@ -36,5 +40,63 @@ class FonnteWhatsappNotificationTest extends TestCase
                 && ($data['message'] ?? null) === 'Pesan pengingat STNK'
                 && ($data['countryCode'] ?? null) === '62';
         });
+    }
+
+    public function test_whatsapp_recipients_are_normalized_for_fonnte(): void
+    {
+        config([
+            'services.fonnte.country_code' => '62',
+        ]);
+
+        $attribute = new CustomAssetAttribute;
+        $attribute->forceFill([
+            'notification_recipient_whatsapp_numbers' => [
+                '+62 812-3456-7890',
+                '0812 3456 7890',
+                '6281234567890',
+            ],
+        ]);
+
+        $command = app(SendScheduledNotifications::class);
+        $method = new ReflectionMethod($command, 'resolveWhatsappRecipients');
+        $method->setAccessible(true);
+
+        $this->assertSame(['6281234567890'], $method->invoke($command, $attribute));
+    }
+
+    public function test_same_whatsapp_reminder_is_not_sent_more_than_once_per_day(): void
+    {
+        CarbonImmutable::setTestNow('2026-07-23 09:00:00');
+        Cache::flush();
+
+        config([
+            'services.fonnte.endpoint' => 'https://api.fonnte.com/send',
+            'services.fonnte.token' => 'secret-token',
+            'services.fonnte.country_code' => '62',
+        ]);
+
+        Http::fake([
+            'https://api.fonnte.com/send' => Http::response(['status' => true], 200),
+        ]);
+
+        $attribute = new CustomAssetAttribute([
+            'notification_channels' => [CustomAssetAttribute::CHANNEL_WHATSAPP],
+            'notification_recipient_whatsapp_numbers' => ['081234567890'],
+        ]);
+        $attribute->id = 77;
+
+        $asset = new Asset(['name' => 'Mobil Operasional']);
+        $asset->id = 55;
+
+        $command = app(SendScheduledNotifications::class);
+        $method = new ReflectionMethod($command, 'sendNotification');
+        $method->setAccessible(true);
+
+        $method->invoke($command, 'Pesan pengingat STNK', $asset, $attribute, 'Pengingat STNK');
+        $method->invoke($command, 'Pesan pengingat STNK', $asset, $attribute, 'Pengingat STNK');
+
+        Http::assertSentCount(1);
+
+        CarbonImmutable::setTestNow();
     }
 }
