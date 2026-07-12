15c6d37efab0e258df72da7803635e0f12c78944
apriansyahrs <apriansyahrizs@gmail.com>
2026-07-12 12:43:51 +0700

    refactor: disable default password reset and profile features in Filament admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -44,6 +44,8 @@ class AdminPanelProvider extends PanelProvider
             ->sidebarLivewireComponent(MekayaSidebar::class)
             ->viteTheme('resources/css/filament/admin/theme.css')
             ->login(Login::class)
+            ->passwordReset(null, null)
+            ->profile(null)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
